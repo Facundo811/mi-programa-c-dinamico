a2c47f67335303f584ebc6baa2df5a2962fb1178
Facundo <facundo.ponzo@alumnos.frm.utn.edu.ar>
2026-09-23 01:37:32 -0300

    refactor: mensaje final de programa y liberación de memoria

--- main.c
+++ main.c
@@ -30,6 +30,7 @@ int main() {
 	// Es muy importante liberar la memoria dinámica cuando ya no se necesita para evitar fugas de memoria.
 	free(arr);
 	printf("Memoria liberada.\n");
-	
+	printf("Programa finalizado y memoria liberada.\n");
+	printf("Programa finalizado y memoria liberada.\n");
 	return 0; // Terminación exitosa del programa
 }
